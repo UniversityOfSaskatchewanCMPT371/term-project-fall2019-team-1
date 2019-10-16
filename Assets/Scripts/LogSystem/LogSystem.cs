6d45695ed42e45ed6214c835e80e51844f8bf4e5
James Scarrow <jrjs1996@hotmail.com>
2019-10-16 17:13:04 -0600

    Log system able to get time and log what was said
    Writes the given text with the current time to the log file.
    Displays the log in the UI.

--- Assets/Scripts/LogSystem/LogSystem.cs
+++ Assets/Scripts/LogSystem/LogSystem.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEditor;
 using System.IO;
+using System;
+
 
 public class LogSystem : MonoBehaviour
 {
     // Text file used for logging. Drag and drop file in editor.
     public TextAsset logFile;
 
-    // For writing to log file
-    private StreamWriter sw;
+    public Text UIText;
 
     // Start is called before the first frame update
     void Start()
@@ -18,17 +20,28 @@ public class LogSystem : MonoBehaviour
         // Clear the log file when scene starts.
         File.WriteAllText(AssetDatabase.GetAssetPath(logFile), string.Empty);
 
-        // Build stream writer for the log file.
-        sw = new StreamWriter(AssetDatabase.GetAssetPath(logFile));
+        writeToFile("yeet");
+        writeToFile("asdfsadvcxbvcx");
+        writeToFile("Last");
 
-        sw.WriteLine("Yeet");
-        sw.Close();
-
+        printToTextField();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void writeToFile(string text)
     {
-        
+        // Build stream writer for the log file.
+        StreamWriter sw = new StreamWriter(AssetDatabase.GetAssetPath(logFile), append: true);
+        // Prepend time to text.
+        string finalAnswer = DateTime.Now.ToString("h:mm:ss tt") + ": " + text;
+
+        sw.WriteLine(finalAnswer);
+        sw.Close();
+    }
+
+    public void printToTextField()
+    {
+        StreamReader sr = new StreamReader(AssetDatabase.GetAssetPath(logFile));
+        UIText.GetComponent<Text>().text = sr.ReadToEnd();
+        sr.Close();
     }
 }
