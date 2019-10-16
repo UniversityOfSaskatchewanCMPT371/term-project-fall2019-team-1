de40de4957a33e8cc9e546b3f6376bded1372eda
James Scarrow <jrjs1996@hotmail.com>
2019-10-16 16:50:59 -0600

    Began working on log system.
    Able to read and write to log file and clear text file with LogSystem
    class. Added SpeechToTextSystem class that converts speech to text.

---
+++ Assets/Scripts/LogSystem/LogSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class LogSystem : MonoBehaviour
+{
+    // Text file used for logging. Drag and drop file in editor.
+    public TextAsset logFile;
+
+    // For writing to log file
+    private StreamWriter sw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Clear the log file when scene starts.
+        File.WriteAllText(AssetDatabase.GetAssetPath(logFile), string.Empty);
+
+        // Build stream writer for the log file.
+        sw = new StreamWriter(AssetDatabase.GetAssetPath(logFile));
+
+        sw.WriteLine("Yeet");
+        sw.Close();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SpeechToTextSystem/SpeechToTextSystem.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class SpeechToTextSystem : MonoBehaviour
+{
+    [SerializeField]
+    private Text hypothesis;
+
+    [SerializeField]
+    private Text recognitions;
+
+    private DictationRecognizer dictationRecognizer;
+
+    // Start is called before the first frame update
+    public void StartSpeechToText()
+    {
+        dictationRecognizer = new DictationRecognizer();
+
+        recognitions.text = "Say a phrase into the audio device...";
+        if (hypothesis)
+            hypothesis.text = "";
+
+        dictationRecognizer.DictationResult += (text, confidence) =>
+        {
+            recognitions.text = text + "\n";
+        };
+
+        dictationRecognizer.DictationHypothesis += (text) =>
+        {
+            if (hypothesis)
+                hypothesis.text += text;
+        };
+
+        dictationRecognizer.DictationComplete += (completionCause) =>
+        {
+            if (completionCause != DictationCompletionCause.Complete)
+                Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", completionCause);
+        };
+
+        dictationRecognizer.DictationError += (error, hresult) =>
+        {
+            Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
+        };
+
+        dictationRecognizer.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
