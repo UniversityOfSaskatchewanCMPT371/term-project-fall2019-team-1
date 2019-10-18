bfc83ff7905e0fe13f9cbd51bb758140c488be5a
Scarrow <jrs678@usask.ca>
2019-10-18 12:40:51 -0600

    Integrate speech, log, and dialogue components
    Created master scene with all components integrated. It recognizes
    speech from the microphone, sends it to the dialogue tree, and the
    correcct prompt is given.

--- Assets/Scripts/SpeechToTextSystem/SpeechToTextSystem.cs
+++ Assets/Scripts/SpeechToTextSystem/SpeechToTextSystem.cs
@@ -10,6 +10,8 @@ public class SpeechToTextSystem : MonoBehaviour
 
     public Text text;
 
+    public DialogueTree dialogueTree;
+
     private string phraseSpoken;
 
     public string PhraseSpoken
@@ -41,6 +43,7 @@ public class SpeechToTextSystem : MonoBehaviour
             // Write the text to the log.
             GameObject.FindGameObjectWithTag("Log").GetComponent<LogSystem>().WriteToFile(phraseSpoken);
             this.text.text = phraseSpoken;
+            dialogueTree.inTree(phraseSpoken);
         };
 
         text.gameObject.SetActive(false);
