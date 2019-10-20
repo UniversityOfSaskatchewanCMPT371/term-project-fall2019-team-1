d409fdc2eb4626a81fec05f6bd07d2e8b6f12562
Mathew Cathcart <cathcartmathew@protonmail.com>
2019-10-20 17:25:55 -0600

    Added Test Hooks

--- Assets/Scripts/GUITree/DialogeTree_Test.cs
+++ Assets/Scripts/GUITree/DialogeTree_Test.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DialogeTree_Test : MonoBehaviour
 {
-    public string test;
+    public string test = "hello one";
 
     // Start is called before the first frame update
     void Start()
---
+++ Assets/Scripts/TestHooks/NPCSpeechTesting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SpeechLib;
+
+public class NPCSpeechTesting : MonoBehaviour
+{
+    SpVoice voice;
+    public Button btn;
+    public InputField input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        voice = new SpVoice();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onPressed()
+    {
+        voice.Speak(input.GetComponentInChildren<Text>().text);
+    }
+}
