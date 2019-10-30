32fde0274df13d74d865b4dcd81f1f84adc980e2
Mason Demerais <mason.demerais@usask.ca>
2019-10-29 18:09:38 -0600

    Added vrtk prototype scene and scene assets

---
+++ Assets/Scripts/OpacityChanger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+public class OpacityChanger : MonoBehaviour
+{
+    public Material target;
+    public void UpdateOpacity(float alphaValue)
+    {
+        Color color = target.color;
+        color.a = alphaValue;
+        target.color = color;
+    }
+}
