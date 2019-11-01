bc9870148ba4bd1b04419b947ca59c43db0a2e83
Mason Demerais <mason.demerais@usask.ca>
2019-10-31 20:04:24 -0600

    Added stubs

--- Assets/PatientSystem/Scripts/LanguageEngine.cs
+++ Assets/PatientSystem/Scripts/LanguageEngine.cs
@@ -2,17 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This is takes in a String from SpeechToText, and uses it to traverse the TreeUI.
+/// </summary>
 public class LanguageEngine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// The tree UI, setup in the inspector, or mocked.
+    /// </summary>
+    public TreeUI treeUI;
+
+    /// <summary>
+    /// The system logger.
+    /// </summary>
+    public Log log;
+
+    /// <summary>
+    /// The system debug logger.
+    /// </summary>
+    public DebugLog debugLog;
+
+    /// <summary>
+    /// Recieves input from the SpeechToText output.
+    /// </summary>
+    /// <param name="input">The input from the stt.</param>
+    public void RecieveInput(string input)
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Will return the best decision given the input string and the options.
+    /// </summary>
+    /// <param name="input">A string to compare to the options.</param>
+    /// <param name="options">The options, an array of array of strings.</param>
+    /// <returns>The index of the option to be taken.
+    /// </returns>
+    public int BestDecision(string input, string[][] options)
     {
-        
+        return 0;
     }
 }
