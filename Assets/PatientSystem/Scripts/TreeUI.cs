cdea52dab3773387a1bc9d0fcaab4334827e99da
Mason Demerais <mason.demerais@usask.ca>
2019-10-31 20:10:26 -0600

    Added stubs to tree ui communcator

--- Assets/PatientSystem/Scripts/TreeUI.cs
+++ Assets/PatientSystem/Scripts/TreeUI.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This communicates with the tree UI. Gets and sets data to it.
+/// </summary>
 public class TreeUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Returns the options we can current take.
+    /// </summary>
+    /// <returns>The options we can take at the current node.</returns>
+    public string[][] GetCurrentOptions()
     {
-        
+        return null;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Traverses the tree down the coresponding index.
+    /// </summary>
+    /// <param name="option">The branch to take.</param>
+    public void TakeOption(int option)
     {
-        
+
+    }
+
+    /// <summary>
+    /// Returns the prompt at the current node we are at.
+    /// </summary>
+    /// <returns>The prompt at the current node.</returns>
+    public string GetCurrentPrompt()
+    {
+        return null;
     }
 }
