093334e6ff6fff37632580e8fde237f0658fa3a9
Sam Horovatin <shorovatin@live.ca>
2019-11-11 13:59:27 -0600

    Created Groovy Scripts
    Create groovy scripts for:
    - Full Test Suite (Daily 9PM)
    - Unity Build (On Demand)

---
+++ vars/FullTestSuite9PM.groovy
@@ -0,0 +1,29 @@
+@Library('shared-library')_
+
+pipeline {
+    agent any
+    environment {
+        BranchToBeBuild = '*/develop' //Indicates what branch will be build by Unity Builder
+        JobName = 'Full Test Suite'
+        UnityBuildResults = ''
+    }
+    stages {
+        stage('Build') {
+            steps {
+                script {
+                    UnityBuildResults = build(job: JobName, parameters: [string(name: 'BRANCH', value: BranchToBeBuild)], propagate: true, wait: true) 
+                    println UnityBuildResults.getRawBuild().getLog()
+                }
+                
+            }
+        }
+    }
+    post {
+        always {
+            script {
+                slackNotifier UnityBuildResults.result.toString()
+                cleanWs()
+            }
+        }
+    }
+}
